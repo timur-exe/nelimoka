d2c92e0458ba8a57a432eb6d5928ade9d59bdcde
timur-exe <151888707+timur-exe@users.noreply.github.com>
2025-11-02 01:12:02 +0300

    Update gigagpt.py

--- Ruslan-Isaev/modules/gigagpt.py
+++ Ruslan-Isaev/modules/gigagpt.py
@@ -34,9 +34,9 @@ class AliceGPT(loader.Module):
             
             if "Начинаю творить, вернусь через несколько секунд" in response1.text:
              response2 = await conv.wait_event(events.NewMessage(incoming=True, from_users=chat))
-             await message.client.send_file(chat, photo)
-            await message.client.send_file(message.to_id, response2.media))
-             await response.delete()
+              await message.client.send_file(chat, photo)
+              await message.client.send_file(message.to_id, response2.media))
+              await response.delete()
              await response1.delete()
              await response2.delete()
              return
